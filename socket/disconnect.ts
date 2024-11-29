782a15b029949fbb3d3ef9eefd4deccfed4b5aa5
Yuwang Cai <mrcaidev@gmail.com>
2024-11-29 14:44:44 +0800

    fix: disconnect event should not send to myself

--- socket/disconnect.ts
+++ socket/disconnect.ts
@@ -2,10 +2,10 @@ import { nanoid } from "nanoid";
 import type { Context } from "./types";
 import { getRoom } from "./utils";
 
-export function handleDisconnect({ io, socket }: Context) {
+export function handleDisconnect({ socket }: Context) {
   const room = getRoom(socket);
 
-  io.to(room).emit("user:left", {
+  socket.broadcast.to(room).emit("user:left", {
     id: nanoid(),
     user: socket.data.user,
     leftTime: Date.now(),
