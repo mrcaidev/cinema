12f3addf704dcf66d4cab70dcd4addbcb0aa25be
Yuwang Cai <mrcaidev@gmail.com>
2024-11-30 20:19:02 +0800

    perf: mark all handlers as async

--- socket/handlers/disconnect.ts
+++ socket/handlers/disconnect.ts
@@ -1,6 +1,6 @@
 import type { Context } from "../types";
 
-export function handleDisconnect({ io, socket }: Context) {
+export async function handleDisconnect({ io, socket }: Context) {
   io.to(socket.data.room).emit("user:left", {
     id: crypto.randomUUID(),
     user: socket.data.user,
--- socket/handlers/ping.ts
+++ socket/handlers/ping.ts
@@ -1,7 +1,7 @@
 import type { ClientToServerEvents } from "@/common/types";
 import type { Context } from "../types";
 
-export function handlePing(
+export async function handlePing(
   _: Context,
   ...args: Parameters<ClientToServerEvents["ping"]>
 ) {
